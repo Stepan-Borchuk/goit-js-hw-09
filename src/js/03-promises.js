1255b9451af9324632a76c20722047280053c0bc
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-05-21 21:13:23 +0300

    10

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,66 +14,66 @@ const refs = {
   submitBtn: document.querySelector('button')
 } 
 
-refs.submitBtn.addEventListener('mouseover', () => {
-  if (refs.delay.value <= 0 || refs.amount.value <= 0 || refs.step.value <= 0) {
-    Notify.failure("Enter positive Values");
-    refs.submitBtn.disabled = true;
-    setTimeout(()=>{refs.submitBtn.disabled = false}, 1000)
-  }
-})
+// refs.submitBtn.addEventListener('mouseover', () => {
+//   if (refs.delay.value <= 0 || refs.amount.value <= 0 || refs.step.value <= 0) {
+//     Notify.failure("Enter positive Values");
+//     refs.submitBtn.disabled = true;
+//     setTimeout(()=>{refs.submitBtn.disabled = false}, 1000)
+//   }
+// })
 
 
 
 // ---- працюэ без промысу----------
 
 
-refs.submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const firstDelay = Number(refs.delay.value);
-  const amount = Number(refs.amount.value);
-  const delayStep = Number(refs.step.value);
-  refs.submitBtn.disabled = true;
-  let position = 0;
-  // createDelay
-  let delay = firstDelay;
-  // console.log(firstDelay);
-  // console.log(amount);
-  // console.log(delayStep);
+// refs.submitBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const firstDelay = Number(refs.delay.value);
+//   const amount = Number(refs.amount.value);
+//   const delayStep = Number(refs.step.value);
+//   refs.submitBtn.disabled = true;
+//   let position = 0;
+//   // createDelay
+//   let delay = firstDelay;
+//   // console.log(firstDelay);
+//   // console.log(amount);
+//   // console.log(delayStep);
   
 
-  const interval = setInterval(() => {
+//   const interval = setInterval(() => {
 
-    if (position === amount) {
-      clearInterval(interval);
-      refs.submitBtn.disabled = false;
-      return
-    }
-    // console.log(delay += delayStep);
-    position += 1
-    delay += delayStep;
-    // console.log(position);
-    // console.log(delay);
+//     if (position === amount) {
+//       clearInterval(interval);
+//       refs.submitBtn.disabled = false;
+//       return
+//     }
+//     // console.log(delay += delayStep);
+//     position += 1
+//     delay += delayStep;
+//     // console.log(position);
+//     // console.log(delay);
 
-    createPromise(position, delay)
+//     createPromise(position, delay)
     
-  }, delay)
+//   }, delay)
   
-});
+// });
 
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // console.log('✅ Fulfille');
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // console.log('✅ Fulfille');
+//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 
-    // return { position, delay }
-  } else {
-    // console.log('❌ Rejected');
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    // return { position, delay }
-  }
-}
+//     // return { position, delay }
+//   } else {
+//     // console.log('❌ Rejected');
+//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//     // return { position, delay }
+//   }
+// }
 
 // -------- ^ працюэ без промысу ^ --------------
 
@@ -236,13 +236,13 @@ function createPromise(position, delay) {
 
 // refs.submitBtn.addEventListener('click', (e) => {
 //   e.preventDefault();
-// const promise = new Promise((resolve, reject) => {
+
   
 // const firstDelay = Number(refs.delay.value);
 //   const amount = Number(refs.amount.value);
 //   const delayStep = Number(refs.step.value);
 //   refs.submitBtn.disabled = true;
-//   // let position = 0;
+//   let position = 0;
 //   // createDelay
 //   let delay = firstDelay;
 //   // console.log(firstDelay);
@@ -250,55 +250,176 @@ function createPromise(position, delay) {
 //   // console.log(delayStep);
   
 
-//   // const interval = setInterval(() => {
+//   const interval = setInterval(() => {
 
-//   //   if (position === amount) {
-//   //     clearInterval(interval);
-//   //     refs.submitBtn.disabled = false;
-//   //     return
-//   //   }
-//   //   // console.log(delay += delayStep);
-//   //   position += 1
-//   //   delay += delayStep;
-//   //   console.log(position);
-//   //   console.log(delay);
+//     if (position === amount) {
+//       clearInterval(interval);
+//       refs.submitBtn.disabled = false;
+//       return
+//     }
+//     // console.log(delay += delayStep);
+//     position += 1
+//     delay += delayStep;
+//     console.log(position);
+//     console.log(delay);
 
-//   //   // createPromise(position, delay)
-//   //     const isSuccess = Math.random() > 0.3;
-//   //   if (isSuccess) {
-//   //     console.log(position);
-//   //     console.log(delay);
-//   //     resolve({ position, delay })
-//   //   } else {
-//   //     console.log(position);
-//   //     console.log(delay);
-//   //     reject({ position, delay })
-//   //   }
-    
+//     createPromise(position, delay).then(({ position, delay }) => {
+//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+//   }).catch(({ position, delay }) => {
+//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+//     })
 
-
-//   //   promise.then(({ position, delay }) => {
-//   //   Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-//   // }).catch(({ position, delay }) => {
-//   //   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-//   //   })
-
-//   // }, delay)
+//   }, delay)
 
 //   // console.log(position);
 //   //   console.log(delay);
 
-//   for (let position = 0; position <= amount; position += 1) {
-//     delay += delayStep;
-//     setTimeout(() => {
-//       console.log(position)
-//       console.log(delay)
-//     }, delay)
-//   }
+
 
 // })
   
   
+
+
+
+// function createPromise() {
+//   return new Promise((resolve, reject) => {
+//   const isSuccess = Math.random() > 0.3;
+//   if (isSuccess) {
+//     resolve({ position, delay })
+//   } 
+//     reject({ position, delay })
+//   }
+//     );
+//   }
+
+// ------- варіант 4 --------------
+
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
 // });
 
+// promise
+//   .then(value => {
+//     console.log(value);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
 
+// const delay = 1000;
+// // const isSuccess = Math.random() > 0.3;
+// let position = 1;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve({ position, delay });
+//     } else {
+//       reject({ position, delay });
+//     }
+//   }, delay);
+// });
+
+// createPromise(position, delay).then(({ position, delay }) => {
+//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+//   }).catch(({ position, delay }) => {
+//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+//   });
+
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    // setTimeout(() => { 
+       if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+    // }, delay)
+   })  
+}
+
+// console.log(position)
+// console.log(delay)
+//   let delay = 1000;
+// // const isSuccess = Math.random() > 0.3;
+// let position = 0;  
+
+
+// setInterval(() => {
+
+//   console.log(position)
+//   console.log (delay)
+  
+//   position += 1;
+//   delay += 1000;
+
+  
+//   createPromise(position, delay).then(({ position, delay }) => {
+//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+//   }).catch(({ position, delay }) => {
+//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+//   });
+//  console.log(position)
+//   console.log (delay)
+
+// }, delay)
+ 
+
+
+refs.submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  
+  const firstDelay = Number(refs.delay.value);
+  const amount = Number(refs.amount.value);
+  const delayStep = Number(refs.step.value);
+  refs.submitBtn.disabled = true;
+  let position = 0; 
+  //   // createDelay
+  //   let delay = firstDelay;
+  //   // console.log(firstDelay);
+  //   // console.log(amount);
+  //   // console.log(delayStep);
+
+  setTimeout(() => {
+    let delay = firstDelay;
+    const interval = setInterval(() => {
+        if (position === amount) {
+      clearInterval(interval);
+      refs.submitBtn.disabled = false;
+      return
+    }
+
+  console.log(position)
+  console.log (delay)
+  
+  position += 1;
+      delay += delayStep;
+
+  
+  createPromise(position, delay).then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+  }).catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+  });
+ console.log(position)
+  console.log (delay)
+
+}, delayStep)
+
+  }, firstDelay)
+
+
+})
